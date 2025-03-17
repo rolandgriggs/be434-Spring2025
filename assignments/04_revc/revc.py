649bdc86a960b150885f7188c3c8fc499a2641a4
Bonnie Hurwitz <bhurwitz@arizona.edu>
2025-03-17 12:52:02 -0700

    fixing filepath issue in revc.py and test.py typo

--- assignments/04_revc/revc.py
+++ assignments/04_revc/revc.py
@@ -22,9 +22,10 @@ def get_args():
 
 def reverse_complement(dna):
     """Return the reverse complement of a DNA sequence"""
+    # only problem is the maketrans doesn't account for the case of the character
+    
     complement = str.maketrans('ACGTacgt', 'TGCAtgca')
     return dna.translate(complement)[::-1]
-
 
 # --------------------------------------------------
 def main():
@@ -32,7 +33,9 @@ def main():
     args = get_args()
 
     # Construct file path in 'inputs/' directory
-    file_path = os.path.join('inputs', args.DNA)
+    #file_path = os.path.join('inputs', args.DNA)
+    # the user is expected to provide the file path on the command line
+    file_path = args.DNA
 
     # Try reading as a file; if fails, treat as sequence
     if os.path.isfile(file_path):
