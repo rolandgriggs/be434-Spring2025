008999125aba8214e09abb0bc71b3ef10a8e8115
Bonnie Hurwitz <bhurwitz@arizona.edu>
2025-03-21 21:46:09 +0000

    turning in 04_revc

--- assignments/04_revc/revc.py
+++ assignments/04_revc/revc.py
@@ -1,53 +1,44 @@
 #!/usr/bin/env python3
 """
-Author : Xavier Griggs
-Date   : 2025-03-02
+Author :Xavier Griggs
+Date   : 2025-03-14
 Purpose: Add Your Purpose
 """
 
 import argparse
 import os
+
 # --------------------------------------------------
 def get_args():
-    """Get command-line arguments"""
     parser = argparse.ArgumentParser(
         description='Print the reverse complement of DNA',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('DNA',
+    parser.add_argument('dna',
                         metavar='DNA',
+                        type=str,
                         help='Input sequence or file')
 
-    return parser.parse_args()
+    args = parser.parse_args()
 
+    if os.path.isfile(args.dna):
+        args.dna = open(args.dna).read().rstrip()
+
+    return args
+
+
+# --------------------------------------------------
 def reverse_complement(dna):
-    """Return the reverse complement of a DNA sequence"""
-    # only problem is the maketrans doesn't account for the case of the character
-    
     complement = str.maketrans('ACGTacgt', 'TGCAtgca')
     return dna.translate(complement)[::-1]
 
+
 # --------------------------------------------------
 def main():
-    """Main execution"""
     args = get_args()
+    dna = args.dna
 
-    # Construct file path in 'inputs/' directory
-    #file_path = os.path.join('inputs', args.DNA)
-    # the user is expected to provide the file path on the command line
-    file_path = args.DNA
-
-    # Try reading as a file; if fails, treat as sequence
-    if os.path.isfile(file_path):
-        with open(file_path, 'rt') as fh:
-            dna_seq = fh.read().strip()
-    else:
-        dna_seq = args.DNA.strip()
-
-    # Print reverse complement
-    print(reverse_complement(dna_seq))
-
-
+    print(reverse_complement(dna))
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
